91919c56c9253512aebdc3fbecb1481aa59c2ef8
Verneva Ziga <verneva.ziga@gmail.com>
2025-05-05 17:41:10 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Verneva Ziga on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
